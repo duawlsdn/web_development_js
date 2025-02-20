76ee22e984e386546dda87217c3d306d0955aa21
duawlsdn <wlsdn893@naver.com>
2025-02-20 21:38:51 +0900

    feat: 20250220 iphone_memo_app

--- chapter14_iPone_memo/script/clock.js
+++ chapter14_iPone_memo/script/clock.js
@@ -1,5 +1,12 @@
 function clcokRun() {
   const phoneClock = document.querySelector('.phone-clock');
+  setInterval(() => {
+    const now = new Date();
+    const nowHours = now.getHours();
+    const nowMinutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+    const nowClockText = `${nowHours}:${nowMinutes}`;
+    phoneClock.innerHTML = nowClockText;
+  }, 1000);
 }
 
 clcokRun();
